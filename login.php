632452c3303de3db422a0c8d9e2755df732793c9
Oguzhan Yigit <ruck.tobias@gmail.com>
2015-04-20 11:21:05 +0200

    initial login system without register function

--- login.php
+++ login.php
@@ -1,40 +1,76 @@
 <?php
-$verbindung = mysql_connect("localhost", "jemline2", "121212");
-mysql_select_db("jemline2");
-
-$username = $_POST["username"];
-$passwort = $_POST["passwort"];
-$passwort2 = $_POST["passwort2"];
-
-if($passwort != $passwort2 OR $username == "" OR $passwort == "")
-    {
-    echo "Eingabefehler. Bitte alle Felder korekt ausfüllen. <a href=\"eintragen.html\">Zurück</a>";
-    exit;
-    }
-$passwort = md5($passwort);
-
-$result = mysql_query("SELECT id FROM login WHERE username LIKE '$username'");
-$menge = mysql_num_rows($result);
-
-if($menge == 0)
-    {
-    $eintrag = "INSERT INTO login (username, passwort) VALUES ('$username', '$passwort')";
-    $eintragen = mysql_query($eintrag);
-
-    if($eintragen == true)
-        {
-        echo "Benutzername <b>$username</b> wurde erstellt. <a href=\"login.html\">Login</a>";
-        }
-    else
-        {
-        echo "Fehler beim Speichern des Benutzernames. <a href=\"eintragen.html\">Zurück</a>";
-        }
-
-
-    }
-
-else
-    {
-    echo "Benutzername schon vorhanden. <a href=\"eintragen.html\">Zurück</a>";
-    }
+session_start();
+if(!empty($_SESSION))
+{
+  header('Location: index.php');
+}
 ?>
+<html>
+    <head>
+        <title>JemLine</title>
+        <link rel="stylesheet" href="includes/style.css">
+        <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+        <script src="js/jquery.min.js"></script>
+        <script src="js/jquery.ui.shake.js"></script>
+<script>
+$(document).ready(function()
+{
+
+$('#login').click(function()
+{
+var username=$("#username").val();
+var password=$("#password").val();
+var dataString = 'username='+username+'&password='+password;
+if($.trim(username).length>0 && $.trim(password).length>0)
+{
+$.ajax({
+type: "POST",
+url: "ajaxLogin.php",
+data: dataString,
+cache: false,
+beforeSend: function(){ $("#login").val('Connecting...');},
+success: function(data){
+if(data)
+{
+$("body").load("index.php").hide().fadeIn(1500).delay(6000);
+
+    if (typeof (history.pushState) != "undefined") {
+        var obj = { Page: "index", Url: "index.php" };
+        history.pushState(obj, obj.Page, obj.Url);
+    } else {
+        alert("Browser does not support HTML5.");
+    }
+
+}
+else
+{
+//Shake animation effect.
+$('#box').shake();
+$("#login").val('Login')
+$("#error").html("<span style='color:#cc0000'>Error:</span> Invalid username and password. ");
+}
+}
+});
+
+}
+return false;
+});
+
+});
+</script>
+</head>
+<div id="body">
+<div id="box">
+<form action="ajaxLogin.php" method="post">
+Username
+<input type="text" class="input"  id="username"/>
+Password
+<input type="password" class="input"  id="password"/>
+<input type="submit" class="button button-primary" value="Log In" id="login"/>
+<div id="error"></div>
+</div>
+</form>
+</div>
+
+</body>
+</html>

f574d13e997c326c29c5e3e5f6bc05e62c282bce
Oguzhan Yigit <ruck.tobias@gmail.com>
2015-05-07 13:56:18 +0200

    fixed fuck

--- contact.php
+++ contact.php
@@ -1,29 +1,29 @@
-<?php 
+<?php
 session_start();
 $thisPage="contact";
-include "includes/header.php"; 
-?> 
+include "includes/header.php";
+?>
 
-<div id="content">
+<div id="content" style="background-color:transparent;">
     <div id="contact">
 	</div>
-	<span style="color: black; text-shadow: rgba(0, 0, 0, 0.3) 0px 0px 4px; display: inline-block; margin:10px 10px 0 0; padding:5px 10px; text-align:center; width:100%">
-		JemLine Jewelry <br /
+	<span style="color: black; text-shadow: rgba(0, 0, 0, 0.3) 0px 0px 4px; display: inline-block; margin:10px 10px 0 0; text-align:center; width:100%">
+		Alfikra Corp. <br /
 		26 Tennant Street <br />
-		Birminham <br />
+		Birmingham <br />
 		B15 2TT <br />
 		United Kingdom <br />
-                <a href="mailto:info@JemLine.co.uk" style="margin-left:auto;margin-right:auto;">E-mail us!</a>
+                <a href="mailto:info@Alfikra.co.uk" style="margin-left:auto;margin-right:auto;">E-mail us!</a>
 	</span>
 </div>
 
 
-    
-    
+
+
 </body>
 
 
 
-<?php 
-include "includes/footer.php"; 
-?> 
+<?php
+include "includes/footer.php";
+?>

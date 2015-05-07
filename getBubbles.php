1c558f4157d6125b066fb9fc94e5ec3b96af03a5
Oguzhan Yigit <ruck.tobias@gmail.com>
2015-05-07 14:10:15 +0200

    Hure

--- getBubbles.php
+++ getBubbles.php
@@ -1,1 +1,12 @@
 <?php
+$db = mysql_connect("localhost", "jemline2", "121212");
+mysql_select_db("jemline2");
+
+$result=mysql_query("SELECT * FROM bubbles");
+$rows = mysql_num_rows($result);
+
+
+while ($row = mysql_fetch_object($result)) {
+    echo json_encode($row);
+}
+?>

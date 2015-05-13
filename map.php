112135d40c7c316c3705dce847bd64087af8f239
Oguzhan Yigit <ruck.tobias@gmail.com>
2015-05-13 11:56:22 +0200

    did some rework

--- map.php
+++ map.php
@@ -1,37 +1,20 @@
-<?php 
+<?php
 $thisPage="map";
 session_start();
-include "includes/header.php"; 
-?> 
+include "includes/header.php";
+?>
 
 <div id="content">
-    <div id="map1">
-		<img src="img/europe.png" width="800px" height="auto" />
-		<div id="germany">
-			<div style="margin-left: -32px;">Germany</div>
-		</div>
-		<div id="spain">
-			<div style="margin-left: -35px;">Spain</div>
-		</div>
-		<div id="mazedonien">
-			<div style="margin-left: -75px;">Macedonia</div>
-		</div>
-    </div>
-	<div style="padding-left: 10px">
-		Lorem ipsum dolor sit amet, etur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, 
-		sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum 
-		dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna 
-		aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-	</div>
+
 </div>
 
 
-    
-    
+
+
 </body>
 
 
 
-<?php 
-include "includes/footer.php"; 
-?> 
+<?php
+include "includes/footer.php";
+?>
